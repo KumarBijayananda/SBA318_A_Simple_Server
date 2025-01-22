47d6562c15eb5bd4f16185035f5d8269bcbf75e8
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-01-21 19:50:49 -0500

    initial commit with ignore list

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express();
+const PORT = 3000;
+
+
+
+//-----------Middleware-------------------
+
+
+
+
+
+//--------------Routes--------------------
+
+
+
+
+
+//--------------Server---------------------
+app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`)
+})
